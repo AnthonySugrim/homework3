4ebe2658a98bb2e767326ca70ea1f2fbb2cdcd02
stardust191 <57815678+stardust191@users.noreply.github.com>
2020-02-23 22:34:30 -0500

    2

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
-
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -11,23 +10,25 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 // creat a generate-password function
+
 function generatePassword() {
-  var pLength = prompt("How long do you want the password to be? choose between 8-128")
+  var pLength = prompt("How long do you want the password to be? choose between 8-128");
   var valid = false;
   while (!valid) {
       if ( pLength <= 8 || pLength >= 128 || isNaN(parseInt(pLength))) {
-          pLength = prompt("Invalid choise try again. choose between 8-128")
+          pLength = prompt("Invalid choise try again. choose between 8-128");
       }
       else {
           valid = true;
       }
-  }
+  } 
+  var password = "";
   // create prompt 
    var specialCharacters = confirm("Do you want special characters?");
    var upperCaseCharacters = confirm("Do you want Upper case letters?");
-   var lowerCaseCharacters = confirm("Do you want use lower case letters?");
-   var numberCharacters = confirm("Do you want use numbers?");
-
+   var lowerCaseCharacters = confirm("Do you want lower case letters?");
+   var numberCharacters = confirm("Do you want numbers?");
+  
   // create if statements for prompt
    if (specialCharacters) {
      var symbols = "!#$%&'()*+,-./:;<=>?@^[]^_`{|}~";
@@ -41,15 +42,21 @@ function generatePassword() {
    if (numberCharacters) {
      var numbers = "0123456789";
    }
-   // 
-   var all = uppercase + lowercase + numbers + symbols;
+   
+   if (specialCharacters == false && upperCaseCharacters == false && lowerCaseCharacters == false && numberCharacters == false)
+   {
+     alert ("Well No password for you then :P");
+   }
+   else {
+     var all = uppercase + lowercase + numbers + symbols;
        //console.log(all);
-    var password = "";
+
     for (var i = 0; i < pLength; i++) {
        var randomNumber = Math.floor(Math.random() * all.length);
        //console.log(randomNumber);
        password += all.substring(randomNumber,randomNumber + 1);
        //console.log(password)
     }
+  }
     return password;
 }
